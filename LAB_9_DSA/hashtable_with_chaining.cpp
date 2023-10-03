38f34d63af923c0e54dc85868860c0b7bdb08c67
Dulmi21 <yapaymdp.21@uom.lk>
2023-10-03 21:14:30 +0530

    Update hashtable_with_chaining.cpp
    changed

--- LAB_9_DSA/hashtable_with_chaining.cpp
+++ LAB_9_DSA/hashtable_with_chaining.cpp
@@ -1,105 +1,115 @@
 #include <iostream>
 #include "linkedlist.cpp"
+
 using namespace std;
 
-struct HashTable{
+struct HashTable
+{
     int MAX_LENGTH = 4;
     int MAX_CHAIN_LENGTH = 2;
-    LinkedList * password = new LinkedList[MAX_LENGTH];
-    bool isFull(){
+    LinkedList *password = new LinkedList[MAX_LENGTH];
+    bool isFull()
+    {
         bool full = true;
         int count = 0;
-        for (int i = 0; i < MAX_LENGTH; i++){
-            if(password[i].head == NULL){
+        for (int i = 0; i < MAX_LENGTH; i++)
+        {
+            if (password[i].head == NULL)
+            {
                 full = false;
             }
         }
         return full;
     }
-    int hashfunc(string user_name){
+    int hashfunc(string user_name)
+    {
         int sum = 0;
         int hash = 0;
-        for(int i = 0;i< user_name.length(); i++){
+        for (int i = 0; i < user_name.length(); i++)
+        {
             sum += int(user_name[i]);
-
         }
         hash = sum % MAX_LENGTH;
 
         return hash;
     }
-    bool is_slot_empty(int hash){
+    bool is_slot_empty(int hash)
+    {
         bool empty;
-        if(password[hash].head==NULL){
+        if (password[hash].head == NULL)
+        {
             empty = true;
         }
-        else{
-            empty=false;
+        else
+        {
+            empty = false;
         }
         return empty;
     }
-    void insert(string user_name,string user_password){
+    void insert(string user_name, string user_password)
+    {
         int hash;
         bool empty;
         hash = hashfunc(user_name);
         empty = is_slot_empty(hash);
-        if(password[hash].length<MAX_CHAIN_LENGTH){
-            password[hash].insert(user_name.data(),user_password.data());
-
+        if (password[hash].length < MAX_CHAIN_LENGTH)
+        {
+            password[hash].insert(user_name.data(), user_password.data());
         }
-        else{
-            cout<<"Linked List reached MAX CAP!\n";
+        else
+        {
+            cout << "Linked List reached MAX CAP!\n";
         }
     }
-    void print_hashtable(){
-        for(int i=0;i<MAX_LENGTH;i++){
-            cout<<"["<<i<<"]-->";
+    void print_hashtable()
+    {
+        for (int i = 0; i < MAX_LENGTH; i++)
+        {
+            cout << "[" << i << "]-->";
             password[i].print_list();
         }
     }
-    void hash_lookup(string user_name){
+    void hash_lookup(string user_name)
+    {
         int hash;
         bool empty;
         hash = hashfunc(user_name);
         empty = is_slot_empty(hash);
-           if (empty){
+        if (empty)
+        {
             cout << "Nothing occupied";
         }
-        else{
+        else
+        {
             password[hash].search(user_name);
-
-
         }
 
-
-	//add your code below
-
-
-
-
-
-
+        // add your code below
     }
 };
 
-int main(){
-    cout<<"This is running"<<endl;
-    HashTable * hashtbl = new HashTable;
-    cout<< hashtbl->isFull()<<"\n";
-    int command=0;
+int main()
+{
+    cout << "This is running" << endl;
+    HashTable *hashtbl = new HashTable;
+    cout << hashtbl->isFull() << "\n";
+    int command = 0;
     string user_name;
     string password;
-    while (command!=-1){
+    while (command != -1)
+    {
         /* code */
         cout << "Type command: ";
         cin >> command;
-        switch (command){
+        switch (command)
+        {
         case 1:
             /* insert the new item*/
             cout << "Enter user name: ";
             cin >> user_name;
             cout << "Enter password to be saved: ";
             cin >> password;
-            hashtbl->insert(user_name,password);
+            hashtbl->insert(user_name, password);
             break;
         case 2:
             /* hash lookup password*/
@@ -115,9 +125,7 @@ int main(){
             hashtbl->print_hashtable();
             cout << "Exiting...\n";
             break;
-
         }
-
     }
     return 0;
 }
